540827f09675c9b077e81f4a49ecb3962c807a1b
VitorDxD <vitornunes2747@gmail.com>
2024-07-27 20:18:52 +0100

    translating form errors messages

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -15,13 +15,23 @@ class ContatoController extends Controller
     }
 
     public function salvar(Request $request) {
-        $request->validate([
+        $configs = [
             'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
+
+        $feedbacks = [
+            'required' => 'O campo :attribute é obrigatório',
+            'min' => 'O campo :attribute precisa ter no mínimo :min caracteres',
+            'max' => 'O campo :attribute precisa ter no máximo :max caracteres',
+            'unique' => 'O :attribute já foi utilizado anteriormente',
+            'email' => 'O email passado não é válido'
+        ];
+
+        $request->validate($configs, $feedbacks, ['motivo_contatos_id' => 'motivo contato']);
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
