2dd588deeca0fe24fbbec3cec41e62c6b0a88ccc
VitorDxD <vitornunes2747@gmail.com>
2024-07-27 19:57:18 +0100

    styling form errors 2

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -2,11 +2,17 @@
 <form action="{{ route('site.contato') }}" method="POST">
     @csrf
     <input name="nome" value="{{ old('nome') }}" type="text" placeholder="Nome" class="{{ $classe }}">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
+
     <input name="telefone" value="{{ old('telefone') }}" type="text" placeholder="Telefone" class="{{ $classe }}">
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
+
     <input name="email" value="{{ old('email') }}" type="text" placeholder="E-mail" class="{{ $classe }}">
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
+
     <select name="motivo_contatos_id" class="{{ $classe }}">
         <option value="">Qual o motivo do contato?</option>
 
@@ -16,18 +22,12 @@
             </option>
         @endforeach
     </select>
+    {{ $errors->has('motivo_contatos_id') ? $errors->first('motivo_contatos_id') : '' }}
     <br>
+
     <textarea name="mensagem" class="{{ $classe }}" placeholder="Preencha aqui a sua mensagem">{{ (old('mensagem') != '') ? old('mensagem') : "" }}</textarea>
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
+
     <button type="submit" class="{{ $classe }}">ENVIAR</button>
 </form>
-
-@if($errors->any())
-
-    <div style="position: absolute; top: 0; left: 0; width: 100%; background: red; text-align: center;">
-        @foreach($errors->all() as $erro)
-            {{ $erro }} <br>
-        @endforeach
-    </div>
-
-@endif
