03a354fdf6ab1ab16d8b137f62b28b65b79bcd90
VitorDxD <vitornunes2747@gmail.com>
2024-07-28 13:43:58 +0100

    permanence of access log

--- app/Http/Middleware/LogAcessoMiddleware.php
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Models\LogAcesso;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,6 +17,11 @@ class LogAcessoMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         //return $next($request);
+        $ip = $request->server->get('REMOTE_ADDR');
+        $route = $request->getRequestUri();
+
+        LogAcesso::create(['log' => "Ip $ip acessou a rota $route"]);
+
         return Response('Chegamos no middleware e finalizamos no próprio middleware.');
     }
 }
