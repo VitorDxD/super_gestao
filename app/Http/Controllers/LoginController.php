df8e5d0b2f36899096fb34a4b56b1875c3044a17
VitorDxD <vitornunes2747@gmail.com>
2024-07-30 13:37:34 +0100

    login validation and old values

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,19 @@ class LoginController extends Controller
         return view('site.login');
     }
 
-    public function autenticar() {
-        return 'Chegamos até aqui!';
+    public function autenticar(Request $request) {
+        $configs = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        $feedbacks = [
+            'usuario.email' => 'O campo precisa ser preenchido com um e-mail válido',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        $request->validate($configs, $feedbacks);
+
+        print_r($request->all());
     }
 }
